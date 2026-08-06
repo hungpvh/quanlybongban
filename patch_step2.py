72b0e6bc8f80fa4ab2b29e2c438a1e69b3e72b56
hungpvh <hungpvh@gmail.com>
2026-08-06 15:35:14 +0700

    Create data branch and Initial empty commit

--- patch_step2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("/app/applet/index.html", "r", encoding="utf-8") as f:
-    content = f.read()
-
-# Replace openPointModal
-old_modal = r"\s*function openPointModal\(loai_diem\).*?let htmlB =.*?htmlB \+= `</div>`;\s*"
-
-# I will use a more precise regex or simple string replacement.
